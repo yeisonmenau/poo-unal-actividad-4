b5d746fedec99eb6f89e68cebe64f4de346b47ce
yeisonmenau <yeisonandresdh@gmail.com>
2025-06-07 12:04:25 -0500

    Inicio actividad 4

---
+++ 1_ejercicio/main.py
@@ -0,0 +1,4 @@
+from ventana_principal import VentanaPrincipal
+if __name__ == "__main__":
+    ventana = VentanaPrincipal()
+    ventana.mainloop()
---
+++ 1_ejercicio/ventana_principal.py
@@ -0,0 +1,9 @@
+import tkinter
+
+class VentanaPrincipal(tkinter.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Ejercicio 1")
+        self.geometry("350x500")
+        
+
